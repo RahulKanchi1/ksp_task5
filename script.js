6cb2bcb32e8d4b6f9444022064b97a79f078eaa4
RahulKanchi1 <150813823+RahulKanchi1@users.noreply.github.com>
2023-12-12 17:08:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log( 'Get well soon ')
+alert( 'HELLO')
+let number = 23;
+console.log( number);
+
+let number2 = 30;
+console.log( number  + number2 ) 
+console.log( number  ** number2 )
+console.log( number  - number2 )
+console.log( number  * number2 )
+number2 *= 2;
+console.log( number2 )
+ let number3 = '60'
+console.log( number3 == number2 )
+console.log( number3 === number2 )
+number2++;
+console.log(  number2 )
+console.log( number3 > number2 )
